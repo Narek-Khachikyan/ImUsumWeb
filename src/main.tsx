1aeb70e5966c4ae94b8df6f483b0f0e1ec6b29c5
magiccwarss <matem.mayreni@gmail.com>
2023-12-12 16:00:55 +0400

    make all necessary works

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './styles/reset.css';
+import './index.css';
+import { Provider } from 'react-redux';
+import { store } from './app/store.ts';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+   <React.StrictMode>
+      <Provider store={store}>
+         <BrowserRouter>
+            <App />
+         </BrowserRouter>
+      </Provider>
+   </React.StrictMode>,
 );
